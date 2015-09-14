eb496a7df27bc2ff8349f1e079c965abfae09945
Simon Sapin <simon.sapin@exyr.org>
2015-09-14 06:25:42 +0200

    Switch from reference counting to an arena-like scheme for node handles.

--- html5ever/__init__.py
+++ html5ever/__init__.py
@@ -4,40 +4,33 @@ from ._ffi import ffi
 capi = ffi.dlopen(os.path.join(os.path.dirname(__file__), 'libhtml5ever_capi.so'))
 
 
-@ffi.callback('Node* (ParserUserData*, Node*)')
-def clone_node_ref(parser, node):
-    ffi.from_handle(ffi.cast('void*', parser)).refcounts[node] += 1
-    return node
-
-
-@ffi.callback('void (ParserUserData*, Node*)')
-def destroy_node_ref(parser, node):
-    parser = ffi.from_handle(ffi.cast('void*', parser))
-    count = parser.refcounts[node]
-    if count == 1:
-        del parser.refcounts[node]
-    else:
-        parser.refcounts[node] = count - 1
-
-
-CALLBACKS = capi.declare_callbacks(clone_node_ref, destroy_node_ref, ffi.NULL, ffi.NULL)
+CALLBACKS = capi.declare_callbacks(ffi.NULL, ffi.NULL, ffi.NULL, ffi.NULL)
 
 
 class Parser(object):
     def __init__(self):
-        self.refcounts = {}
-        self.self_handle = ffi.new_handle(self)
+        self._keep_alive_handles = []
         self.document = Document()
-        self.ptr = capi.new_parser(CALLBACKS, self.self_handle, self._new_node(self.document))
+        self._ptr = capi.new_parser(
+            CALLBACKS, self._keep_alive(self), self._keep_alive(self.document))
 
     def __del__(self):
         # Do this here rather than through ffi.gc:
         # by the time ffi.gc would trigger, self.refcounts might have been removed already.
-        capi.destroy_parser(self.ptr)
+        capi.destroy_parser(self._ptr)
 
-    def _new_node(self, node):
-        handle = ffi.new_handle(self.document)
-        self.refcounts[handle] = 1
+    def _keep_alive(self, obj):
+        '''
+        Keep the given object alive at least as long as the parser,
+        and return a handle that can go through C / Rust as a `void *` pointer.
+
+        For nodes, we could use a reference counting scheme here with the `clone_node_ref`
+        and `destroy_node_ref` callbacks, but that would only help with nodes
+        created by the parser that don’t end up in the tree (to free them early),
+        which is probably rare if it happens at all.
+        '''
+        handle = ffi.new_handle(obj)
+        self._keep_alive_handles.append(handle)
         return handle
 
 
@@ -45,5 +38,5 @@ class Node(object):
     """Abstract base class for all nodes in the tree."""
 
 
-class Document(object):
+class Document(Node):
     """A document node, the root of the tree."""
