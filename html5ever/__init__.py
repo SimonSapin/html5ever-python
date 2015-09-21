875efdd6d071f3802d38c4b1f6587a82f35bbf3e
Simon Sapin <simon.sapin@exyr.org>
2015-09-22 00:27:56 +0200

    Start testing tree construction against expected serialization.

--- html5ever/__init__.py
+++ html5ever/__init__.py
@@ -59,6 +59,11 @@ class DocumentFragment(Node):
 
 
 HTML_NAMESPACE = 'http://www.w3.org/1999/xhtml'
+MATHML_NAMESPACE = 'http://www.w3.org/1998/Math/MathML'
+SVG_NAMESPACE = 'http://www.w3.org/2000/svg'
+XLINK_NAMESPACE = 'http://www.w3.org/1999/xlink'
+XML_NAMESPACE = 'http://www.w3.org/XML/1998/namespace'
+XMLNS_NAMESPACE = 'http://www.w3.org/2000/xmlns/'
 
 
 class Element(Node):
--- tests/tree_construction.py
+++ tests/tree_construction.py
@@ -1,9 +1,17 @@
 import os.path
-from html5ever import parse
+import pprint
+from html5ever import *
 
 
 def test_tree_construction(test):
-    document = parse(test[b'data'])
+    document = parse(test[b'data'].rstrip(b'\n'))
+    serialized = ''.join(serialize(document)).rstrip('\n')
+    expected = test[b'document'].rstrip(b'\n').decode('utf8')
+    if serialized != expected:
+        pprint.pprint(test)
+        print(serialized)
+        print(expected)
+        assert serialized == expected
 
 
 def pytest_generate_tests(metafunc):
@@ -37,4 +45,69 @@ def parse_tests(fd):
         else:
             value += line
     assert test
+    assert key not in test
+    test[key] = value
     yield test
+
+
+def serialize(node, indent=1):
+    if isinstance(node, Document):
+        for child in node.children:
+            yield from serialize(child, indent)
+        return
+
+    yield '|'
+    yield ' ' * indent
+
+    if isinstance(node, Doctype):
+        yield '<!DOCTYPE '
+        yield node.name
+        if node.public_id or node.system_id:
+            yield ' "%s" "%s"' % (public, system)
+        yield '>\n'
+
+    elif isinstance(node, Text):
+        yield '"'
+        yield node.data
+        yield '"\n'
+
+    elif isinstance(node, Comment):
+        yield '<!-- '
+        yield node.data
+        yield ' -->\n'
+
+    else:
+        assert isinstance(node, Element)
+        yield '<'
+        namespace_url, local_name = node.name
+        if namespace_url == SVG_NAMESPACE:
+            yield 'svg '
+        elif namespace_url == MATHML_NAMESPACE:
+            yield 'math '
+        else:
+            assert namespace_url == HTML_NAMESPACE
+        yield local_name
+        yield '>\n'
+
+        for (namespace_url, local_name), value in sorted(node.attributes.items()):
+            yield '|'
+            yield ' ' * (indent + 2)
+            if namespace_url == XLINK_NAMESPACE:
+                yield 'xlink '
+            elif namespace_url == XML_NAMESPACE:
+                yield 'xml '
+            elif namespace_url == XMLNS_NAMESPACE:
+                yield 'xmlns '
+            else:
+                assert not namespace_url
+            yield '%s="%s"\n' % (local_name, value)
+
+    for child in node.children:
+        yield from serialize(child, indent + 2)
+
+    if isinstance(node, Element) and node.template_contents:
+        yield '|'
+        yield ' ' * (indent + 2)
+        yield 'content\n'
+        for child in node.template_contents.children:
+            yield from serialize(child, indent + 4)
